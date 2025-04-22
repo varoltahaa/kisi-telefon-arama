560e0bcae7102209b36e7e2bcb4258368696319d
Taha <tahaavarol@hotmail.com>
2025-04-22 09:06:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def search():
     return render_template('results.html', name=name, phone=phone, results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
